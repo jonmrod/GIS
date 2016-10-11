59eef216f390971d98a7938ed4fadbeb76da1647
Jonathan Rodriguez <jonathanrodriguez224@gmail.com>
2016-10-11 03:39:42 -0400

    remove images on overlay, add distance between two points

--- GIS/www/js/controllers.js
+++ GIS/www/js/controllers.js
@@ -22,128 +22,188 @@ angular.module('app.controllers', [])
 })
 
 .controller('AccountCtrl', function(Upload, $scope, GeoData) {
-  var file = {};
-  $scope.picFile = {};
-  $scope.onFileSelect = function($files) {
-    file = $scope.picFile = $files[0];
+  var file1 = {};
+  var file2 = {};
+  $scope.pic1 = {};
+  $scope.pic2 = {};
+  $scope.distance = null;
+
+  $scope.selectFirstFile = function($files) {
+    $scope.distance = null;
+    file1 = $scope.pic1 = $files[0];
     var reader = new FileReader();
-    reader.readAsDataURL(file);
+    reader.readAsDataURL(file1);
     reader.onload = function(e) {
-     $scope.$apply(function(){
-      $scope.ImageSrc = e.target.result;
-    });  
-   }
- } 
- $scope.submitGeo = function() {
-  EXIF.getData(file, function() {
-    console.log(EXIF.pretty(this));
-    var lati = EXIF.getTag(this, 'GPSLatitude');
-    var long = EXIF.getTag(this, 'GPSLongitude');
-    var latiRef = EXIF.getTag(this, 'GPSLatitudeRef') || "N";
-    var longRef = EXIF.getTag(this, 'GPSLongitudeRef') || "W";
-    var orien = EXIF.getTag(this, 'GPSInfoIFDPointer');
-    lati = (lati[0] + lati[1]/60 + lati[2]/3600) * (latiRef == "N" ? 1 : -1); 
-    long = (long[0] + long[1]/60 + long[2]/3600) * (longRef == "W" ? -1 : 1);
-    startMap(lati,long, orien);
-  });
-}
-function displayImage (coord) {
-    // startMap(res.data)
-    console.log(res);
+      EXIF.getData(file1, function() {
+        console.log(EXIF.pretty(this));
+        var lati = EXIF.getTag(this, 'GPSLatitude');
+        var long = EXIF.getTag(this, 'GPSLongitude');
+        var latiRef = EXIF.getTag(this, 'GPSLatitudeRef') || "N";
+        var longRef = EXIF.getTag(this, 'GPSLongitudeRef') || "W";
+        var orien = EXIF.getTag(this, 'GPSInfoIFDPointer');
+        lati = (lati[0] + lati[1]/60 + lati[2]/3600) * (latiRef == "N" ? 1 : -1); 
+        long = (long[0] + long[1]/60 + long[2]/3600) * (longRef == "W" ? -1 : 1);
+        file1 = {
+          lat: lati,
+          lng: long,
+          orien: orien
+        }
+      });
+      $scope.$apply(function() {
+        $scope.ImageSrc = e.target.result;
+      });  
+    }
+  } 
+  $scope.selectSecondFile = function($files) {
+    $scope.distance = null;
+    file2 = $scope.pic2 = $files[0];
+    var reader = new FileReader();
+    reader.readAsDataURL(file2);
+    reader.onload = function(e) {
+      EXIF.getData(file2, function() {
+        console.log(EXIF.pretty(this));
+        var lati = EXIF.getTag(this, 'GPSLatitude');
+        var long = EXIF.getTag(this, 'GPSLongitude');
+        var latiRef = EXIF.getTag(this, 'GPSLatitudeRef') || "N";
+        var longRef = EXIF.getTag(this, 'GPSLongitudeRef') || "W";
+        var orien = EXIF.getTag(this, 'GPSInfoIFDPointer');
+        lati = (lati[0] + lati[1]/60 + lati[2]/3600) * (latiRef == "N" ? 1 : -1); 
+        long = (long[0] + long[1]/60 + long[2]/3600) * (longRef == "W" ? -1 : 1);
+        file2 = {
+          lat: lati,
+          lng: long,
+          orien: orien
+        }
+      });
+      $scope.$apply(function() {
+        $scope.ImageSrc = e.target.result;
+      });  
+    }
+  } 
+  $scope.submitGeo = function() {
+    if (file1 && file2) {
+      console.log(file1);
+      console.log(file2);
+      startMap(file1, file2);
+      var p1 = new google.maps.LatLng(file1.lat, file1.lng);
+      var p2 = new google.maps.LatLng(file2.lat, file2.lng);
+      $scope.distance = google.maps.geometry.spherical.computeDistanceBetween(p1, p2) / 1609.344;
+    }
   }
-  function startMap (lati, long, orien) {
-    var lat = lati;
-    var lng = long;
-    var latLng = new google.maps.LatLng(lat, lng);
-    var overlay;
-    USGSOverlay.prototype = new google.maps.OverlayView();
+  function startMap (file1, file2) {
+    var lat = file1.lat;
+    var lng = file1.lng;
+    var bound = new google.maps.LatLngBounds();
+    bound.extend( new google.maps.LatLng(file1.lat, file1.lng));
+    bound.extend( new google.maps.LatLng(file2.lat, file2.lng));
+    // var overlay;
+    // USGSOverlay.prototype = new google.maps.OverlayView();
 
     var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 15,
-      center: latLng,
+      zoom: 10,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
+    var Symbol1 = {
+      path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+      scale: 5,
+      fillOpacity: 0.0,
+      rotation: file1.orien
+    };
+    new google.maps.Marker({
+      position: new google.maps.LatLng(file1.lat, file1.lng), 
+      map: map,
+      animation: google.maps.Animation.DROP,
+      icon: Symbol1
+    });
+    var Symbol2 = {
+      path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+      scale: 5,
+      fillOpacity: 0.0,
+      rotation: file2.orien
+    };
+    new google.maps.Marker({
+      position: new google.maps.LatLng(file2.lat, file2.lng), 
+      map: map,
+      animation: google.maps.Animation.DROP,
+      icon: Symbol2
+    });
 
-    var bounds = new google.maps.LatLngBounds(
-      new google.maps.LatLng(lat - .0001, lng - .0001),
-      new google.maps.LatLng(lat + .0001, lng + .0001));
+    map.fitBounds(bound);
 
-        // The custom USGSOverlay object contains the USGS image,
-        // the bounds of the image, and a reference to the map.
-        overlay = new USGSOverlay(bounds, $scope.ImageSrc, map);
+    var flightPlanCoordinates = [
+      {lat: file1.lat, lng: file1.lng},
+      {lat: file2.lat, lng: file2.lng}
+    ];
+    var flightPath = new google.maps.Polyline({
+      path: flightPlanCoordinates,
+      geodesic: true,
+      strokeColor: '#FF0000',
+      strokeOpacity: 1.0,
+      strokeWeight: 2
+    });
 
-        /** @constructor */
-        function USGSOverlay(bounds, image, map) {
+    flightPath.setMap(map);
 
-        // Initialize all properties.
-        this.bounds_ = bounds;
-        this.image_ = image;
-        this.map_ = map;
+    // var bounds = new google.maps.LatLngBounds(
+    //   new google.maps.LatLng(lat - .0001, lng - .0001),
+    //   new google.maps.LatLng(lat + .0001, lng + .0001));
 
-        // Define a property to hold the image's div. We'll
-        // actually create this div upon receipt of the onAdd()
-        // method so we'll leave it null for now.
-        this.div_ = null;
+    //     // The custom USGSOverlay object contains the USGS image,
+    //     // the bounds of the image, and a reference to the map.
+    //     overlay = new USGSOverlay(bounds, $scope.ImageSrc, map);
 
-        // Explicitly call setMap on this overlay.
-        this.setMap(map);
-      }
+    //     /** @constructor */
+    //     function USGSOverlay(bounds, image, map) {
 
-      USGSOverlay.prototype.onAdd = function() {
+    //     // Initialize all properties.
+    //     this.bounds_ = bounds;
+    //     this.image_ = image;
+    //     this.map_ = map;
 
-        var div = document.createElement('div');
-        div.style.borderStyle = 'none';
-        div.style.borderWidth = '0px';
-        div.style.position = 'absolute';
+    //     // Define a property to hold the image's div. We'll
+    //     // actually create this div upon receipt of the onAdd()
+    //     // method so we'll leave it null for now.
+    //     this.div_ = null;
 
-        // Create the img element and attach it to the div.
-        var img = document.createElement('img');
-        img.src = this.image_;
-        img.style.width = '100%';
-        img.style.height = '100%';
-        img.style.position = 'absolute';
-        img.style.opacity = '0.75';
-        div.appendChild(img);
+    //     // Explicitly call setMap on this overlay.
+    //     this.setMap(map);
+    //   }
 
-        this.div_ = div;
+    //   USGSOverlay.prototype.onAdd = function() {
 
-        // Add the element to the "overlayLayer" pane.
-        var panes = this.getPanes();
-        panes.overlayLayer.appendChild(div);
-      };
+    //     var div = document.createElement('div');
+    //     div.style.borderStyle = 'none';
+    //     div.style.borderWidth = '0px';
+    //     div.style.position = 'absolute';
 
-      USGSOverlay.prototype.draw = function() {
+    //     // Create the img element and attach it to the div.
+    //     var img = document.createElement('img');
+    //     img.src = this.image_;
+    //     img.style.width = '100%';
+    //     img.style.height = '100%';
+    //     img.style.position = 'absolute';
+    //     img.style.opacity = '0.75';
+    //     div.appendChild(img);
 
-        var overlayProjection = this.getProjection();
-        var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
-        var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
+    //     this.div_ = div;
 
-        var div = this.div_;
-        div.style.left = sw.x + 'px';
-        div.style.top = ne.y + 'px';
-        div.style.width = (ne.x - sw.x) + 'px';
-        div.style.height = (sw.y - ne.y) + 'px';
-      };
-      var mySymbol = {
-        path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
-        scale: 5,
-        fillOpacity: 0.0,
-        rotation: orien
-      };
-      var marker = new google.maps.Marker({
-       position: latLng,
-       map: map,
-       animation: google.maps.Animation.DROP,
-       icon: mySymbol
-     });
-      var infoWindow = new google.maps.InfoWindow({
-        content: 'lat:' + lat + '<br>lon:' + lng
-      });
+    //     // Add the element to the "overlayLayer" pane.
+    //     var panes = this.getPanes();
+    //     panes.overlayLayer.appendChild(div);
+    //   };
 
-      google.maps.event.addListener(marker, 'click', function () {
-        infoWindow.open(map, marker);
-      });
+    //   USGSOverlay.prototype.draw = function() {
 
+    //     var overlayProjection = this.getProjection();
+    //     var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
+    //     var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
+
+    //     var div = this.div_;
+    //     div.style.left = sw.x + 'px';
+    //     div.style.top = ne.y + 'px';
+    //     div.style.width = (ne.x - sw.x) + 'px';
+    //     div.style.height = (sw.y - ne.y) + 'px';
+    //   };
     }
     DisplayGeoData = function(res) {
       EXIF = res;
